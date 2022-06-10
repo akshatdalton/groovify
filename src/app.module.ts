9681397eeef0cbed09c9070c381c456476912392
akshatdalton <akshat@markovml.com>
2022-06-11 03:17:41 +0530

    Add dummy `play` command.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PlayController } from "./play/play.controller";
+<<<<<<< HEAD
 import { PlayService } from './play/play.service';
 import { PlayModule } from './play/play.module';
 
@@ -9,5 +10,12 @@ import { PlayModule } from './play/play.module';
     imports: [PlayModule],
     controllers: [AppController, PlayController],
     providers: [AppService, PlayService],
+=======
+
+@Module({
+    imports: [],
+    controllers: [AppController, PlayController],
+    providers: [AppService],
+>>>>>>> f364f2e (Add dummy `play` command.)
 })
 export class AppModule {}
