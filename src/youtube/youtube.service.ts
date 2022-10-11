8957f20b628324449797ece6e265d8419fbdcd58
Akshat <akshat25iiit@gmail.com>
2022-10-12 02:09:50 +0530

    Add `YouTubeResults` interface.

--- src/youtube/youtube.service.ts
+++ src/youtube/youtube.service.ts
@@ -7,6 +7,13 @@ import { get_base_filepath } from "src/utils/utils";
 import * as ytMusic from "node-youtube-music";
 import { MusicVideo } from "node-youtube-music";
 
+interface YouTubeResults {
+    url: string;
+    title: string;
+    duration: string;
+    thumbnailUrl: string;
+}
+
 @Injectable()
 export class YoutubeService {
     async searchQuery(query: string) {
@@ -17,7 +24,7 @@ export class YoutubeService {
         // TODO: Define a common structure/interface for
         // what should be returned by `getYouTubeVideosByQuery`
         // function and `getYouTubeMusicByQuery` function.
-        let results = [];
+        let results: YouTubeResults[] = [];
         try {
             // package: `node-youtube-music` uses api key to search,
             // so we must be careful about youtube quota limit.
@@ -25,34 +32,43 @@ export class YoutubeService {
         } catch (e) {
             try {
                 results = await this.getYouTubeVideosByQuery(query);
-                results = this.mapYouTubeVideosToMusics(results);
             } catch (e) {}
         }
-        return results.slice(0, 5);
+        return results;
     }
 
-    private mapYouTubeVideosToMusics(videos: any[]) {
+    private mapYouTubeVideosToMusics(videos: any[]): YouTubeResults[] {
         // There is a slight chance that this modification
         // would not work depending on the video if it's
         // YouTube music version is available or not.
-        const musics = videos.map((video) => ({
-            ...video,
+        const musics: YouTubeResults[] = videos.map((video) => ({
             url: `https://music.youtube.com/watch?v=${video.id.videoId}`,
+            title: video.title,
+            duration: video.duration_raw,
+            thumbnailUrl: video.snippet.thumbnails.url,
         }));
         return musics;
     }
 
-    private async getYouTubeVideosByQuery(query: string) {
+    private async getYouTubeVideosByQuery(
+        query: string,
+    ): Promise<YouTubeResults[]> {
         const videos = await yt.search(query);
-        return videos;
+        return this.mapYouTubeVideosToMusics(videos);
     }
 
-    private async getYouTubeMusicByQuery(query: string) {
+    private async getYouTubeMusicByQuery(
+        query: string,
+    ): Promise<YouTubeResults[]> {
         const musics: MusicVideo[] = await ytMusic.searchMusics(query);
-        return musics.map((music: MusicVideo) => ({
-            url: `https://music.youtube.com/watch?v=${music.youtubeId}`,
-            ...music,
-        }));
+        return musics.map(
+            (music: MusicVideo): YouTubeResults => ({
+                url: `https://music.youtube.com/watch?v=${music.youtubeId}`,
+                title: music.title,
+                duration: music.duration.label,
+                thumbnailUrl: music.thumbnailUrl,
+            }),
+        );
     }
 
     private async downloadYouTubeMusic(
